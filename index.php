fa3fa59cfa33fde5b4a297f96c88cf7eca4d8e91
MartinUbl <martinubl@seznam.cz>
2017-12-14 10:49:47 +0100

    Some testing contents

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>iCe Online: web deployment test project</title>
+</head>
+
+<body>
+	<?php
+	echo "This is test project used for testing Anveena web deployment. Date: ".date("Y-m-d H:i:s");
+	?>
+</body>
+
+</html>
