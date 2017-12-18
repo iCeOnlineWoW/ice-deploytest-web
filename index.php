d55a94a4e84fb01055a856d9877f3fbf4bdb0347
MartinUbl <cmaranec@seznam.cz>
2017-12-18 14:30:26 +0100

    Added composer file for composer build step testing

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
 
 <body>
 	<?php
-	echo "This is test project used for testing Anveena web deployment. Date: ".date("Y-m-d H:i:s");
+	
+	require __DIR__.'/vendor/autoload.php';
+	
+	// just to test composer build step
+	use MathPHP\Probability\Combinatorics;
+	$perm = Combinatorics::permutations(5);
+	
+	echo "This is test project used for testing Anveena web deployment. Date: ".date("Y-m-d H:i:s")."<br/>";
+	echo "5! = ".$perm;
 	?>
 </body>
 
